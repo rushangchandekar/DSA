78817d02f35567434a9fcdbb7609e5877b170194
RushangChandekar <142429467+rushangchandekar@users.noreply.github.com>
2026-07-04 19:45:52 +0530

    Added the BST

---
+++ BST.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == NULL) {
+        return new Node(val);
+    }
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> arr) {
+    Node* root = NULL;
+    for (int val : arr) {
+        root = insert(root, val);
+    }
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+bool search(Node* root, int key){
+    if(root == NULL){
+        return false;
+    }
+    if(root->data == key){
+        return true;
+    }
+    if(root->data > key){
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
+Node* getInorderSuccessor(Node* root) {
+    while (root && root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* delNode(Node* root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (key < root->data) {
+        root->left = delNode(root->left, key);
+    } else if (key > root->data) {
+        root->right = delNode(root->right, key);
+    } else {
+        // root == key, delete this node
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        } else {
+            Node* IS = getInorderSuccessor(root->right);
+            root->data = IS->data;
+            root->right = delNode(root->right, IS->data);
+        }
+    }
+    return root;
+}
+
+int main() {
+    vector<int> arr = {3, 2, 1, 5, 6, 4};
+
+    Node* root = buildBST(arr);
+    inorder(root);
+    cout << endl;
+    // cout << search(root, 8) << endl;
+    root = delNode(root, 5);
+    
+    inorder(root);
+    cout << endl;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+cout << search(root, 8) << endl;
